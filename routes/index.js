18616cbf4c312cff4ff8d5acaeee0e2abfa53396
Serhii_Nikitin <Vverbatim123>
2018-10-23 15:45:07 +0300

    prepare to Deploy

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const customers = require('./customers');
 const movies = require('./movies');
 const rentals = require('./rentals');
 const users = require('./users');
+const returns = require('./returns');
 
 router.use('/genres', genres);
 router.use('/customers', customers);
 router.use('/movies', movies);
 router.use('/rentals', rentals);
 router.use('/users', users);
+router.use('/returns', returns);
 
 module.exports = router;
--- routes/rentals.js
+++ routes/rentals.js
@@ -6,10 +6,7 @@ const rentalController = require('../controllers').rentalController;
 router.get('/', rentalController.getAll);
 router.get('/:id', rentalController.getById);
 
-// router.post('/', rentalController.create);
-router.post('/', async (req, res, next) => {
-  res.status(401).send('Unautorized');
-});
+router.post('/', rentalController.create);
 router.delete('/:id', rentalController.delete);
 
 module.exports = router;
